6f2774079b3eb40d9076518d944cf7ed11962ec1
Magnum Adelano <magnemcs@gmail.com>
2019-08-27 20:17:13 -0400

    Adicionando biblioteca para acesso ao Amazon S3 e configuração de acessos na API.

--- src/test/java/com/portfolio/magnum/VideoconverterApplicationTests.java
+++ src/test/java/com/portfolio/magnum/VideoconverterApplicationTests.java
@@ -16,7 +16,6 @@ import ws.schild.jave.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
